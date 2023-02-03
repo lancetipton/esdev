2d0499a95e3ac522575c0f079b4b7a31ee215e4a
Lance Tipton <lancetipton04@gmail.com>
2023-02-03 16:45:41 -0700

    fix: copy d.ts types

--- configs/build.config.ts
+++ configs/build.config.ts
@@ -1,6 +1,7 @@
 import path from 'node:path'
 import * as esbuild from 'esbuild'
 import { fileURLToPath } from 'url'
+import { promises as fs } from 'fs'
 import { getAllFiles } from 'get-all-files'
 import { dTSPathAliasPlugin } from 'esbuild-plugin-d-ts-path-alias'
 
@@ -8,10 +9,17 @@ const dirname = path.dirname(fileURLToPath(import.meta.url))
 const Root = path.join(dirname, `..`)
 const outdir = path.join(Root, `dist`)
 const srcDir = path.join(Root, `src`)
+const typesDir = path.join(outdir, `src`)
 
 ;(async () => {
+
+  // Remove the existing output dir
+  await fs.rm(outdir, { recursive: true, force: true })
+
+  // Get all paths to be generated
   const entryPoints = await getAllFiles(srcDir).toArray()
 
+  // Build the files with esbuild
   await esbuild.build({
     outdir,
     entryPoints,
@@ -22,9 +30,14 @@ const srcDir = path.join(Root, `src`)
     platform: 'node',
     plugins: [
       dTSPathAliasPlugin({
-        outputPath: Root,
+        outputPath: outdir,
         tsconfigPath: path.join(Root, 'tsconfig.json'),
       })
     ]
   })
+
+  // Copy the types into the correct dir to match the dist output dir
+  await fs.cp(typesDir, outdir, { recursive: true, force: true })
+  await fs.rm(typesDir, { recursive: true, force: true })
+
 })()
